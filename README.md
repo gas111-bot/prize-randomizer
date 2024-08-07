fe547f9ae6367526c02cbed431f428480bfb709a
Karim Iskakov <kar.iskakov@gmail.com>
2024-08-07 23:35:09 +0300

    Final edits

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# prize-randomizer
+# ⛽ GasPump Trading Competition
+Welcome to the GasPump Trading Competition prize selection! For more details about the competition, prizes, and rules, please check our [telegram post](https://t.me/gaspump_tv/1184).
+
+## Random Seed
+The random seed used for selecting the winners will be determined during a livestream on [@gaspump_tv](https://t.me/gaspump_tv) channel. We will roll 5 dice emojis 🎲 to generate a 5-digit number, which will be used as random seed. This ensures complete transparency and fairness in the winner selection process, because Telegram garantees the randomness of the dice.
+
+## How to run and reproduce the results
+1. Install dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+2. Run the code:
+```bash
+python select_winners.py --data data.csv --seed {SEED}
+```
--- select_winners.py
+++ select_winners.py
@@ -1,8 +1,6 @@
 import os
-import time
 import argparse
 import pandas as pd
-import json
 import numpy as np
 
 import design
@@ -11,108 +9,177 @@ import design
 prize_pool = {
     "iPhone 15": {
         "n_items": 15,
-        "price": 750,
+        "price_usdt": 750,
     },
     "PS5 Slim": {
         "n_items": 2,
-        "price": 750,
+        "price_usdt": 750,
     },
     "iPhone 15 Pro": {
         "n_items": 10,
-        "price": 1000,
+        "price_usdt": 1000,
     },
     "Macbook Air M2": {
         "n_items": 2,
-        "price": 1000,
+        "price_usdt": 1000,
     },
     "Apple Vision Pro": {
         "n_items": 1,
-        "price": 4500,
+        "price_usdt": 4500,
     },
 }
+
+
+def _load_data(path):
+    df = pd.read_csv(path)
+
+    # get telegram_id to username mapping
+    telegram_id_to_username = {
+        row['telegram_id']: row['username'] if not pd.isnull(row['username']) else None
+        for _, row in df.iterrows()
+    }
+
+    return df, telegram_id_to_username
+
+
+def _select_winners(df, telegram_id_to_username, seed, save_path):
+    # get tickets
+    tickets = []
+    for _, row in df.iterrows():
+        tickets.extend([row['telegram_id']] * row['tickets_count'])
+
+    # shuffle tickets (randomization goes here)
+    np.random.seed(seed)
+    np.random.shuffle(tickets)
+
+    # select winners
+    winners = []
+    for prize, details in prize_pool.items():
+        for _ in range(details['n_items']):
+            if tickets:
+                winner_telegram_id = tickets.pop()
+                winner_username = telegram_id_to_username.get(winner_telegram_id, None)
+                winners.append({
+                    "telegram_id": winner_telegram_id,
+                    "username": winner_username,
+                    "prize": prize,
+                    "price_usdt": details["price_usdt"]
+                })
+
+    # save winners
+    winners_df = pd.DataFrame(winners)
+    winners_df.to_csv(save_path, index=False)
+
+    return winners
+
+
+def _get_stats(df, telegram_id_to_username):
+    total_n_participants = df.shape[0]
+
+    top_5_participants_df = df.nlargest(5, "tickets_count")
+    top_5_participants = [
+        {
+            "telegram_id": row["telegram_id"],
+            "username": telegram_id_to_username.get(row["telegram_id"], None),
+            "tickets_count": row["tickets_count"],
+        }
+        for _, row in top_5_participants_df.iterrows()
+    ]
+
+    return top_5_participants, total_n_participants
 
 
 def main(data_path, seed):
     # intro
     os.system("clear")
     print(design.GASPUMP)
-    time.sleep(5.0)
+
+    print(f"Random seed: {seed}")
+    print(f"NumPy version: {np.__version__}")
+    print(f"Pandas version: {pd.__version__}")
+    input()
 
     os.system("clear")
     print(design.TRADING_COMPETITION)
-    time.sleep(5.0)
+    input()
 
     os.system("clear")
     print(design.WINNERS_WILL_BE_SELECTED)
-    time.sleep(5.0)
+    input()
 
     os.system("clear")
     print(design.NOW)
-    time.sleep(5.0)
+    input()
 
-    # print lib versions
-    print(f"NumPy version: {np.__version__}")
-    print(f"Pandas version: {pd.__version__}")
+    # load data
+    df, telegram_id_to_username = _load_data(data_path)
 
-    # set the random seed
-    np.random.seed(seed)
-    print(f"Random seed: {seed}")
+    # select winners
+    winners = _select_winners(df, telegram_id_to_username, seed, "./winners.csv")
 
-    # create a list of all tickets
-    data = pd.read_csv(data_path)
-    tickets = []
-    for _, row in data.iterrows():
-        tickets.extend([row['user_id']] * row['n_tickets'])
+    # get stats
+    top_5_participants, total_n_participants = _get_stats(df, telegram_id_to_username)
 
-    # shuffle tickets (randomization goes here)
-    np.random.shuffle(tickets)
-
-    # draw winners
-    time.sleep(5.0)
+    # print stats
     os.system("clear")
+    print(f"Total number of participants:", end="")
+    input()
+    print(f"{total_n_participants}!", end="")
     print()
-    print("Are you ready?")
-    time.sleep(5.0)
-    print("Let's go!")
-    time.sleep(5.0)
+    input()
 
-    winners = {}
-    for prize, details in prize_pool.items():
-        winners[prize] = []
-        for _ in range(details['n_items']):
-            if tickets:
-                winner_user_id = tickets.pop()
-                winners[prize].append(winner_user_id)
+    print("Top 5 (biggest amount of tickets):", end="")
+    for idx, participant in enumerate(top_5_participants, 1):
+        input()
 
-    with open("./winners.json", "w") as f:
-        json.dump(winners, f, indent=4)
+        # build text
+        text = f"{idx}. {participant['telegram_id']}"
+        if participant['username']: text += f" @{participant['username']}"
+        text += f" – {participant['tickets_count']} tickets"
 
-    # output the winners
-    for prize, user_ids in winners.items():
-        prize_n_items = prize_pool[prize]["n_items"]
-        prize_price = prize_pool[prize]["price"]
-        print(f"Prize: {prize} [{prize_n_items}x, ${prize_price} each]")
+        print(text, end="")
+    print()
+    input()
 
-        for user_id in user_ids:
-            time.sleep(1.0)
+    # print winners
+    os.system("clear")
+    print("So...", end="")
+    input()
 
-            # try to get username (could be absent)
-            try:
-                username = data[data['user_id'] == user_id]['username'].values[0]
-            except:
-                username = None
+    print("IT'S TIME!", end="")
+    input()
 
-            # build text
-            text = f"🎉 {user_id}"
-            if username: text += f" ({username})"
-            text += f" wins {prize}!"
-            print(text)
+    print(f"Today we will be giving away {len(winners)} prizes with a total value of ~$30K", end="")
+    input()
 
-        print()
+    print("Good luck and have fun!", end="")
+    print()
+    input()
+
+    print("Here're the luckiest GasPumpers:", end="")
+    print()
+
+    for winner in winners:
+        input()
+
+        telegram_id, username, prize, price_usdt = (
+            winner["telegram_id"],
+            winner["username"],
+            winner["prize"],
+            winner["price_usdt"],
+        )
+
+        # build text
+        text = f"🎉 {telegram_id}"
+        if username: text += f" @{username}"
+        text += f" – wins '{prize}' (${price_usdt})!"
+
+        print(text, end="")
+    print()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Lottery Script')
+    parser = argparse.ArgumentParser(description='GasPump Trading Competition prize selection')
     parser.add_argument('--data', type=str, required=True, help='Path to the .csv file')
     parser.add_argument('--seed', type=int, required=True, help='Random seed')  # get from 5 dice rolls in @gaspump_tv on live stream
     args = parser.parse_args()
